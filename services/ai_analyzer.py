afebaaf0d4d29ae2a4b623ce72e81e0ae89027c8
ahsen ez <ahsen_ezgun@hotmail.com>
2026-08-07 01:08:07 +0300

    Add ai_analyzer.py to services directory

---
+++ services/ai_analyzer.py
@@ -0,0 +1,20 @@
+cv-analyzer/
+
+app.py
+
+services/
+    ai_analyzer.py
+    ats.py
+    parser.py
+
+templates/
+    index.html
+    result.html
+
+static/
+    css/
+    js/
+
+uploads/
+
+requirements.txt
