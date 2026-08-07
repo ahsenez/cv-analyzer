5cb8301b014002c3057473b7bad5606ade47c990
ahsen ez <ahsen_ezgun@hotmail.com>
2026-08-08 00:14:58 +0300

    Refactor imports in app.py for clarity

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import os
-from docx import Document
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-from services.parser import extract_text_from_pdf
+from services.parser import (
+    extract_text_from_pdf,
+    extract_text_from_docx
+)
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "uploads"
